57e5130267e87d27b04f3893b5aa4607eb6ce71c
phrewfuf <me@andreaskist.de>
2014-03-15 16:42:25 +0100

    modded fix-everything.sh to create symlinks to open.sh and close.sh

--- fix-everything.sh
+++ fix-everything.sh
@@ -10,6 +10,16 @@ useradd -b /var/portal -G openclose -m close
 chgrp -R openclose /var/portal/open
 chgrp -R openclose /var/portal/close
 
+#symlink open.sh and close.sh
+ln -s /root/portalv3/software/open/open.sh /var/portal/open/open.sh
+ln -s /root/portalv3/software/close/close.sh /var/portal/close/close.sh
+
+#set permissions to open.sh and close.sh
+chown open:openclose /var/portal/open/open.sh
+chown close:openclose /var/portal/close/close.sh
+chmod 774 /var/portal/open/open.sh
+chmod 774 /var/portal/close/close.sh
+
 #set permissions to access.log
 touch /var/portallog/access.log
 chgrp openclose /var/portallog/access.log
