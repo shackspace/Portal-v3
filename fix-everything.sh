c28f103f12f474c669bf300f386669b53b28c038
phrewfuf <me@andreaskist.de>
2014-03-15 17:23:00 +0100

    better path handling

--- fix-everything.sh
+++ fix-everything.sh
@@ -1,28 +1,35 @@
 #!/bin/sh
 #initial bootstrapping script
 
+#cd to where the script is
+cd $(dirname $0)
+
 #create users and group
 groupadd openclose
-useradd -b /var/portal -G openclose -m open
-useradd -b /var/portal -G openclose -m close
+mkdir /
+useradd -b /home -G openclose -m open
+useradd -b /home -G openclose -m close
 
 #set permissions
-chgrp -R openclose /var/portal/open
-chgrp -R openclose /var/portal/close
+chgrp -R openclose /home/open
+chgrp -R openclose /home/close
+
+#get most recent stuff from github
+git pull
 
 #symlink open.sh and close.sh
-ln -s /root/portalv3/software/open/open.sh /var/portal/open/open.sh
-ln -s /root/portalv3/software/close/close.sh /var/portal/close/close.sh
+ln -s ./open/open.sh /home/open/open.sh
+ln -s ./close/close.sh /home/close/close.sh
 
 #set permissions to open.sh and close.sh
-chown open:openclose /var/portal/open/open.sh
-chown close:openclose /var/portal/close/close.sh
-chmod 774 /var/portal/open/open.sh
-chmod 774 /var/portal/close/close.sh
+chown open:openclose /home/open/open.sh
+chown close:openclose /home/close/close.sh
+chmod 774 /home/open/open.sh
+chmod 774 /home/close/close.sh
 
 #set permissions to access.log
 touch /var/portallog/access.log
-chgrp openclose /var/portallog/access.log
+chgrp openclose /homelog/access.log
 chmod -R 775 /var/portallog
 
 #execute GPIO-init script
