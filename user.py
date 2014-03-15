82a2b748ffc8026bd68ef4380a06c96d9952c593
rixx <git@foyfoy.de>
2014-03-15 21:07:46 +0100

    users can now be added

--- user.py
+++ user.py
@@ -13,7 +13,25 @@ if __name__ == '__main__':
     elif sys.argv[1] == 'delete':
         user_actions.del_user(sys.argv[2])
 
-    elif sys.argv[1] == 'add' or sys.argv[1] == 'modify':
+    elif sys.argv[1] == 'add': 
+        parser = OptionParser()
+
+        parser.add_option('-k', dest='keyfile')
+        parser.add_option('-s', dest='serial')
+        parser.add_option('-n', dest='name')
+        parser.add_option('-l', dest='surname')
+        parser.add_option('--nick', dest='nick', default=None)
+        parser.add_option('--first-valid', dest='first', default=None)
+        parser.add_option('--last-valid', dest='last', default=None)
+    
+        (options, args) = parser.parse_args()
+        user_actions.add_user(keyfile=options.keyfile, serial=options.serial, \
+                              name=options.name, surname=options.surname, \
+                              nickname=options.nick, lastValid=options.last, \
+                              firstValid=options.first)
+        
+
+    elif sys.argv[1] == 'modify':
         pass
 
     else:
--- user_mgmt/user_actions.py
+++ user_mgmt/user_actions.py
@@ -1,4 +1,5 @@
 import db_actions
+import subprocess
 
 
 def list_users():
@@ -41,9 +42,53 @@ def del_user(uid):
             print("User not deleted.")
 
 
+def add_user(serial, keyfile, name, surname, nickname, lastValid, firstValid):
+    cur, conn = db_actions.get_db()
+
+    if not (serial and keyfile and name and surname):
+        print("You must provide a membership number, a name, a surname and\
+               a keyfile.")
+        sys.exit(1)
+
+    if not is_key_valid(keyfile):
+        print("Please provide a valid keyfile.")
+        sys.exit(1)
+
+    try:
+        cur.execute("INSERT INTO user(serial, name, surname, firstValid, \
+                     lastValid, pubkey, nickname) VALUES (?, ?, ?, ?, ?, ?, ?)",\
+                     (serial, name, surname, firstValid, lastValid, \
+                     get_key(keyfile), nickname))
+        conn.commit()
+        conn.close()
+        print("User successfully added.")
+    
+    except:
+        conn.close()
+        print("Could not add user.")
+
+
 def pretty_print(output_list):
     col_width = [max(len(str(x)) for x in col) for col in zip(*output_list)]
 
     for line in output_list:
         print "| " + " | ".join("{:{}}".format(x, col_width[i])
                                 for i, x in enumerate(line)) + " |"
+
+
+def get_key(keyfile):
+    f = open(keyfile, 'r')
+    key = f.read()
+    f.close()
+
+    key = key.strip()
+    return key
+
+
+def is_key_valid(keyfile):
+    try:
+        subprocess.check_call(["ssh-keygen", "-l", "-f", str(keyfile)])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
