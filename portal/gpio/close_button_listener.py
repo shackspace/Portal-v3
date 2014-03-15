0ddb63d5a750f72d34d79ca7ab24a2c7b47133f4
momo <m@hackathon.de>
2014-03-15 22:50:41 +0100

    initial commit

---
+++ portal/gpio/close_button_listener.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+#-*- coding:utf-8 -*-
+
+import RPi.GPIO as GPIO
+
+CLOSEBUTTON = 24
+
+def main():
+    GPIO.add_interrupt_callback(CLOSEBUTTON, callback=close_door, pull_up_down=RPIO.PUD_DOWN)
+    GPIO.wait_for_interrupts()
+
+def close_door():
+    print('Closing door')
+    
+
+if __name__ == '__main__':
+    main()
