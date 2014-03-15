a7b2ca93970a18942733259f735856fb2a83b5a7
ttb <ttb@shackspace.de>
2014-03-15 20:16:07 +0100

    deployment script

---
+++ client/deploy.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2
+#-*- coding: utf8 -*-
+"""Deployscript"""
+
+from os import path
+import os
+from subprocess import call
+from sys import exit
+
+AUTHORIZED_KEYS_PREFIX = "authorized_keys"
+HOMEPATH = "/var/portal"
+HOST = "portal.shack"
+
+
+def main():
+    for state in ['open', 'close']:
+        deploy_authorized_keys(state)
+
+
+def deploy_authorized_keys(state):
+    """push the authorized_keys file to the portal
+
+    also fixes filepermissions"""
+    filename = '.'.join([AUTHORIZED_KEYS_PREFIX, state])
+    if not path.isfile(filename):
+        print("'%s' is not a file" % filename)
+        exit(1)
+    if not os.access(filename, os.R_OK):
+        print("'%s' is not readale" % filename)
+        exit(2)
+
+    connection = "@".join(["root", HOST])
+    destination_path = path.join(HOMEPATH, state, ".ssh", "authorized_keys")
+    target = ":".join([connection, destination_path])
+    command = ["scp", filename, target]
+
+    ret = call(command)
+    if ret > 0:
+        print "scp of '%s' failed" % filename
+        return ret
+
+    remote_command = ["chown", "-v", ":".join([state, state]), destination_path]
+    ret = call(["ssh", connection] + remote_command)
+    if ret > 0:
+        print "chwon of %s failed" % state
+        return ret
+
+    remote_command = ["chmod", "-v", "400", destination_path]
+    ret = call(["ssh", connection] + remote_command)
+    if ret > 0:
+        print "chmod of %s failed" % state
+        return ret
+
+
+if __name__ == "__main__":
+    main()
