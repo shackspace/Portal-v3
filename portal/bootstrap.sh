4d2d52ee2c477694849c450599e97aae363e3d0e
ttb <ttb@shackspace.de>
2015-04-25 04:54:33 +0200

    dns server added

--- portal/bootstrap.sh
+++ portal/bootstrap.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 #initial bootstrapping script
 
+set -e
+
 
 aptitude -y update
-aptitude -y install git hostapd udhcpd python-virtualenv python-dev build-essential
+aptitude -y safe-upgrade
+aptitude -y install git hostapd udhcpd python-virtualenv python-dev build-essential bind9
 
 
 #clone repo
@@ -11,25 +14,24 @@ git clone https://github.com/shackspace/Portal-v3.git /opt/Portal-v3
 
 
 #link config files to /etc
-rm /etc/hostapd/hostapd.conf
-ln -s /opt/Portal-v3/portal/config/hostapd/hostapd.conf /etc/hostapd/hostapd.conf
+ln -sf /opt/Portal-v3/portal/config/hostapd/hostapd.conf /etc/hostapd/hostapd.conf
 
-rm /etc/default/hostapd
-ln -s /opt/Portal-v3/portal/config/default/hostapd /etc/default/hostapd
+ln -sf /opt/Portal-v3/portal/config/default/hostapd /etc/default/hostapd
 
-rm /etc/udhcpd.conf
-ln -s /opt/Portal-v3/portal/config/udhcpd.conf /etc/udhcpd.conf
+ln -sf /opt/Portal-v3/portal/config/udhcpd.conf /etc/udhcpd.conf
 
-rm /etc/default/udhcpd
-ln -s /opt/Portal-v3/portal/config/default/udhcpd /etc/default/udhcpd
+ln -sf /opt/Portal-v3/portal/config/default/udhcpd /etc/default/udhcpd
 
-rm /etc/network/interfaces
-ln -s /opt/Portal-v3/portal/config/network/interfaces /etc/network/interfaces
+ln -sf /opt/Portal-v3/portal/config/network/interfaces /etc/network/interfaces
 
+ln -sf /opt/Portal-v3/portal/config/bind/named.conf.local /etc/bind/named.conf.local
+
+ln -sf /opt/Portal-v3/portal/config/bind/db.portal /etc/bind/db.portal
 
 #restart hostapd and udhcpd
 service hostapd restart
 service udhcpd restart
+service bind9 restart
 
 
 #generate env
@@ -43,17 +45,17 @@ pip install -r requirements.txt
 
 
 #add user open
-useradd -b /home --create-home open
+useradd -b /home --create-home -G dialout open
 mkdir /home/open/.ssh
 chown open:open /home/open/.ssh
 chmod 700 /home/open/.ssh
 
 
 #add user close
-useradd -b /home --create-home close
+useradd -b /home --create-home -G dialout close
 mkdir /home/close/.ssh
 chown close:close /home/close/.ssh
 chmod 700 /home/close/.ssh
 
-echo open portal= NOPASSWD: /opt/Portal-v3/portal/gpio/portal.py >> /etc/sudoers
-echo close portal= NOPASSWD: /opt/Portal-v3/portal/gpio/portal.py >> /etc/sudoers
+#echo open portal= NOPASSWD: /opt/Portal-v3/portal/gpio/portal.py >> /etc/sudoers
+#echo close portal= NOPASSWD: /opt/Portal-v3/portal/gpio/portal.py >> /etc/sudoers
