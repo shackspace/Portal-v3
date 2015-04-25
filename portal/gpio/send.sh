eb3ce439a6d189bf7006a66c47383af3a96e7d79
rashfael <rashfael@isobeef.org>
2015-04-25 18:51:44 +0200

    correctly not-escape variables in send script

--- portal/gpio/send.sh
+++ portal/gpio/send.sh
@@ -7,4 +7,4 @@ ADDRESS='http://127.0.0.1/push'
 /opt/Portal-v3/portal/gpio/check_status.py
 if [ $? -eq 0 ] ; then STATUS='open'; fi;
 
-curl -X POST $IP -d '{"status":"$STATUS", "nick": "$KEYHOLDER"}' -H "Content-type: application/json" $ADDRESS
+curl -X POST $IP -d '{"status":"'$STATUS'", "nick": "'$KEYHOLDER'"}' -H "Content-type: application/json" $ADDRESS
