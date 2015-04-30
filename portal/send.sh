517eaa716ba29ea709cd84649f9ae19f60ff503d
rixx <rixx-git@cutebit.de>
2015-04-30 17:15:11 +0200

    modified to include unknown case

--- portal/send.sh
+++ portal/send.sh
@@ -4,7 +4,13 @@ KEYHOLDER=$(cat /var/log/portal/keyholder)
 STATUS='closed'
 ADDRESS='http://10.0.2.10:8080/push'
 
-#/opt/Portal-v3/portal/check_status.py
-#if [ $? -eq 0 ] ; then STATUS='open'; fi;
+/opt/Portal-v3/portal/check_status.py
+RET=$?
+
+if [ $RET -eq 1 ]; then
+    STATUS='open'; 
+elif [ $RET -eq -1 ]; then
+    STATUS='unknown';
+fi;
 
 curl -X POST -d '{"status":"'$STATUS'", "keyholder": '$KEYHOLDER'}' -H "Content-type: application/json" $ADDRESS
