6a94cab50696dcbfe35d0e073119a6863196d398
ttb <ttb@shackspace.de>
2015-10-26 03:04:47 +0100

    timeout in watchdog sh reduced to 60 sec

--- portal/watchdog.sh
+++ portal/watchdog.sh
@@ -5,7 +5,7 @@ if [ -f $lockfile ]
 then
     age=$(stat -c %Y $lockfile)
     now=$(date +"%s")
-    if (( (now - age) > 120 ))
+    if (( (now - age) > 60 ))
     then
         echo remove lockfile, PID: $(cat $lockfile)
         kill -9 $(cat $lockfile)
