fc4f3001705fa3d338ca0e2fbf027c50ed514afe
ttb <ttb@shackspace.de>
2016-02-20 17:11:50 +0100

    classes Portal and SerialConnection added

--- portal/portal.py
+++ portal/portal.py
@@ -28,32 +28,251 @@ SERTIMEOUT = 1
 LOGLEVEL = 2
 
 
+class SerialCommunication():
+    def __init__(self, serial_port=SERPORT, serial_baud=SERBAUD, serial_timeout=SERTIMEOUT):
+        self.serial_port = serial_port
+        self.serial_baud = serial_baud
+        self.serial_timeout = serial_timeout
+        self.ser = serial.Serial(self.serial_port, self.serial_baud, timeout=self.serial_timeout)
+
+    def __enter__(self):
+        # self.ser = serial.Serial(self.serial_port, self.serial_baud, timeout=self.serial_timeout)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.ser.close()
+
+    def validate_connection(self):
+        if self.ser.closed:
+            self.ser = serial.Serial(self.serial_port, self.serial_baud, timeout=self.serial_timeout)
+
+    def get_pin(self, pin):
+        self.validate_connection()
+        for _ in xrange(10):
+            self.ser.write(str(pin) + " 0\n")
+            ret = self.ser.readline()
+            # org = ret
+            ret = ret.strip()
+            ret = ret.split()
+            if len(ret) < 2:
+                # print ("fail: get to short", pin, org )
+                continue
+            if not ret[0].strip() == str(pin):
+                # print ("fail: get", pin, org)
+                continue
+            state = ret[1]
+            if state.strip() == "0":
+                # print ("get", pin, org, state, False)
+                return False
+            else:
+                # print ("get", pin, org, state, True)
+                return True
+        log("get failded for " + str(pin))
+
+    def set_pin(self, pin, state):
+        self.validate_connection()
+        if state:
+            state = "1"
+        else:
+            state = "0"
+        # print ("set", pin, state)
+
+        for _ in xrange(10):
+            self.ser.write(str(pin) + " " + state + "\n")
+            ret = self.ser.readline()
+
+            # org = ret
+            ret = ret.strip()
+            ret = ret.split()
+            if len(ret) < 2:
+                # print ("fail: set to short", pin, state, org )
+                continue
+            if not ret[0].strip() == str(pin):
+                # print ("fail: set", pin, state, org)
+                continue
+            set_state = ret[1]
+            if set_state.strip() == state:
+                # print ("set", pin, state, org)
+                return
+            else:
+                # print ("fail: set", pin, state, org)
+                continue
+        log("set failded for pin %d with state %s" % (pin, state))
+        # TODO raise
+
+
+class Portal():
+    def __init__(self, serial_port=SERPORT, serial_baud=SERBAUD, serial_timeout=SERTIMEOUT):
+        self.serial = SerialCommunication(serial_port=SERPORT, serial_baud=SERBAUD, serial_timeout=SERTIMEOUT)
+
+    def __enter__(self):
+        self.serial.__enter__()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.serial.__exit__(type, value, traceback)
+
+    def open_portal(self):
+        """
+        Open the door
+        """
+        for _ in xrange(3):
+            print("opening keymatic")
+            self.serial.set_pin(KEYMATIC_OPEN_PIN, True)
+            time.sleep(1)
+            self.serial.set_pin(KEYMATIC_OPEN_PIN, False)
+            time.sleep(5)
+            if self.is_reed_open(15):
+                self.beep_success()
+                return
+            self.beep_fail()
+
+    def is_reed_open(self, timeout=0):
+        status = self.serial.get_pin(DOOR_LOCK_STATE_PIN)
+        for _ in xrange(timeout):
+            status = self.serial.get_pin(DOOR_LOCK_STATE_PIN)
+            log("door lock status: " + str(status), 2)
+            if status:
+                return True
+            time.sleep(1)
+        return status
+
+    def is_reed_closed(self, timeout=0):
+        status = self.serial.get_pin(DOOR_LOCK_STATE_PIN)
+        for _ in xrange(timeout):
+            status = self.serial.get_pin(DOOR_LOCK_STATE_PIN)
+            log("door lock status: " + str(status), 2)
+            if not status:
+                return True
+            time.sleep(1)
+
+        return not status
+
+    def is_door_button_open(self):
+        return self.serial.get_pin(DOOR_STATE_PIN)
+
+    def close_door(self):
+        log("closing keymatic", 2)
+        self.serial.set_pin(KEYMATIC_CLOSE_PIN, True)
+        time.sleep(1)
+        self.serial.set_pin(KEYMATIC_CLOSE_PIN, False)
+
+    def close_portal(self):
+        for _ in xrange(30):
+            self.beep(.5)
+            if not self.is_door_button_open():
+                log("door closed", 2)
+                time.sleep(.5)
+                self.serial.set_pin(KEYMATIC_CLOSE_PIN, True)
+                time.sleep(1)
+                self.serial.set_pin(KEYMATIC_CLOSE_PIN, False)
+                if self.is_reed_closed(15):
+                    self.beep_success()
+                    return
+                else:
+                    break
+            log("door still open", 2)
+            time.sleep(0.5)
+        self.alarm()
+
+    def alarm(self):
+        log("door close failed")
+        self.beep_alarm()
+
+    def beep(self, duration=0.2):
+        self.serial.set_pin(BUZZER_PIN, True)
+        time.sleep(duration)
+        self.serial.set_pin(BUZZER_PIN, False)
+
+    def beep_alarm(self):
+        for _ in xrange(30):
+            self.beep(0.1)
+            time.sleep(0.1)
+
+    def beep_success(self):
+        for _ in xrange(2):
+            self.beep(0.2)
+            time.sleep(0.2)
+
+    def beep_fail(self):
+        self.beep(1)
+
+
+class Lockfile():
+    def __init__(self, lockfile):
+        self.lockfile = lockfile
+        self.create_lock()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.remove_lock()
+
+    def create_lock(self):
+        """
+        create a lockfile
+        """
+        if os.path.isfile(LOCKFILE):
+            f = open(self.lockfile, 'r')
+            content = f.read()
+            content.strip()
+            if self.lockpid_running(content):
+                log('Could not lock open job, locked by %s' % content)
+                sys.exit(1)
+            else:
+                log("Removing lockfile as %s isn't running anymore" % content, 2)
+                self.remove_lock()
+        with open(self.lockfile, 'w') as f:
+            f.write(str(os.getpid()))
+
+    def remove_lock(self):
+        """
+        remove the lock file
+        """
+        try:
+            os.remove(self.lockfile)
+        except OSError:
+            log("Couldn't remove lock file: %s" % self.lockfile)
+
+    def lockpid_running(self, pid):
+        """
+        check if pid is running
+        """
+        try:
+            os.kill(int(pid), 0)
+        except OSError:
+            return False
+        else:
+            return True
+
+
 def main():
-    beep(0.1)
     motd()
     parser = get_option_parser()
     (options, args) = parser.parse_args()
     check_options(options)
-    create_lock(options.name)
-    if options.action == 'open':
-        msg = 'Door opened by: %s (ID: %s)' % (options.name, options.serial)
-        log(msg)
-        open_portal()
-        if options.nick:
-            update_keyholder(options.nick)
-        else:
-            name = options.name
-            name = name.split(' ')
-            name[0] = name[0][:3]
-            if len(name) > 1:
-                name[len(name) - 1] = name[1][:1]
-            name = ' '.join(name)
-            update_keyholder(name)
-    if options.action == 'close':
-        msg = 'Door closed by: %s (ID: %s)' % (options.name, options.serial)
-        log(msg)
-        close_portal()
-    remove_lock()
+    with Lockfile(LOCKFILE):
+        with Portal() as portal:
+            portal.beep(0.1)
+            if options.action == 'open':
+                msg = 'Door opened by: %s (ID: %s)' % (options.name, options.serial)
+                log(msg)
+                if options.nick:
+                    update_keyholder(options.nick)
+                else:
+                    name = options.name
+                    name = name.split(' ')
+                    name[0] = name[0][:3]
+                    if len(name) > 1:
+                        name[len(name) - 1] = name[1][:1]
+                    name = ' '.join(name)
+                    update_keyholder(name)
+                portal.open_portal()
+            if options.action == 'close':
+                msg = 'Door closed by: %s (ID: %s)' % (options.name, options.serial)
+                log(msg)
+                portal.close_portal()
 
 
 def motd():
@@ -70,105 +289,6 @@ def log(message, level=1):
     print(message)
     f = open(LOGFILE, 'a')
     f.write(message + "\n")
-    f.close()
-
-
-def set_pin(pin, state):
-    if state:
-        state = "1"
-    else:
-        state = "0"
-    # print ("set", pin, state)
-
-    for _ in xrange(10):
-        SER = serial.Serial(SERPORT, SERBAUD, timeout=SERTIMEOUT)
-        SER.write(str(pin) + " " + state + "\n")
-        ret = SER.readline()
-        SER.close()
-
-        # org = ret
-        ret = ret.strip()
-        ret = ret.split()
-        if len(ret) < 2:
-            # print ("fail: set to short", pin, state, org )
-            continue
-        if not ret[0].strip() == str(pin):
-            # print ("fail: set", pin, state, org)
-            continue
-        set_state = ret[1]
-        if set_state.strip() == state:
-            # print ("set", pin, state, org)
-            return
-        else:
-            # print ("fail: set", pin, state, org)
-            continue
-    log("set failded for pin %d with state %s" % (pin, state))
-    # TODO raise
-
-
-def get_pin(pin):
-    for _ in xrange(10):
-        SER = serial.Serial(SERPORT, SERBAUD, timeout=SERTIMEOUT)
-        SER.write(str(pin) + " 0\n")
-        ret = SER.readline()
-        SER.close()
-        # org = ret
-        ret = ret.strip()
-        ret = ret.split()
-        if len(ret) < 2:
-            # print ("fail: get to short", pin, org )
-            continue
-        if not ret[0].strip() == str(pin):
-            # print ("fail: get", pin, org)
-            continue
-        state = ret[1]
-        if state.strip() == "0":
-            # print ("get", pin, org, state, False)
-            return False
-        else:
-            # print ("get", pin, org, state, True)
-            return True
-    log("get failded for " + str(pin))
-
-
-def remove_lock():
-    """
-    remove the lock file
-    """
-    try:
-        os.remove(LOCKFILE)
-    except OSError:
-        log("Couldn't remove lock file: %s" % LOCKFILE)
-
-
-def lockpid_running(pid):
-    """
-    check if pid is running
-    """
-    try:
-        os.kill(int(pid), 0)
-    except OSError:
-        return False
-    else:
-        return True
-
-
-def create_lock(name):
-    """
-    create a lockfile
-    """
-    if os.path.isfile(LOCKFILE):
-        f = open(LOCKFILE, 'r')
-        content = f.read()
-        content.strip()
-        if lockpid_running(content):
-            log('Could not lock open job, locked by %s' % content)
-            sys.exit(1)
-        else:
-            log("Removing lockfile as %s isn't running anymore" % content, 2)
-            remove_lock()
-    f = open(LOCKFILE, 'w')
-    f.write(str(os.getpid()))
     f.close()
 
 
@@ -193,12 +313,11 @@ def update_keyholder(name):
     """
     update the status file with the current keyholder
     """
-    f = open(STATUSFILE, 'w')
-    f.write(name)
-    f.close()
-    gid = grp.getgrnam("portal").gr_gid
-    uid = uid = pwd.getpwnam("open").pw_uid
-    os.chown(STATUSFILE, uid, gid)
+    with open(STATUSFILE, 'w') as f:
+        f.write(name)
+        gid = grp.getgrnam("portal").gr_gid
+        uid = uid = pwd.getpwnam("open").pw_uid
+        os.chown(STATUSFILE, uid, gid)
 
 
 def get_option_parser():
@@ -226,104 +345,6 @@ def get_option_parser():
                       help='OPT: first valid day of the key')
     return parser
 
-
-def open_keymatic():
-    print("opening keymatic")
-    set_pin(KEYMATIC_OPEN_PIN, True)
-    time.sleep(1)
-    set_pin(KEYMATIC_OPEN_PIN, False)
-
-
-def open_portal():
-    """
-    Open the door
-    """
-    for _ in xrange(3):
-        open_keymatic()
-        time.sleep(5)
-        if is_reed_open(15):
-            beep_success()
-            return
-        beep_fail()
-
-
-def is_reed_open(timeout=0):
-    status = get_pin(DOOR_LOCK_STATE_PIN)
-    for _ in xrange(timeout):
-        status = get_pin(DOOR_LOCK_STATE_PIN)
-        log("door lock status: " + str(status), 2)
-        if status:
-            return True
-        time.sleep(1)
-
-    return status
-
-
-def is_reed_closed(timeout=0):
-    status = get_pin(DOOR_LOCK_STATE_PIN)
-    for _ in xrange(timeout):
-        status = get_pin(DOOR_LOCK_STATE_PIN)
-        log("door lock status: " + str(status), 2)
-        if not status:
-            return True
-        time.sleep(1)
-
-    return status
-
-
-def is_door_button_open():
-    return get_pin(DOOR_STATE_PIN)
-
-
-def close_door():
-    log("closing keymatic", 2)
-    set_pin(KEYMATIC_CLOSE_PIN, True)
-    time.sleep(1)
-    set_pin(KEYMATIC_CLOSE_PIN, False)
-
-
-def close_portal():
-    for _ in xrange(30):
-        beep(.5)
-        if not is_door_button_open():
-            log("door closed", 2)
-            time.sleep(.5)
-            close_door()
-            if is_reed_closed(15):
-                beep_success()
-                return
-            else:
-                break
-        log("door still open", 2)
-        time.sleep(0.5)
-    alarm()
-
-
-def alarm():
-    log("door close failed")
-    beep_alarm()
-
-
-def beep(duration=0.2):
-    set_pin(BUZZER_PIN, True)
-    time.sleep(duration)
-    set_pin(BUZZER_PIN, False)
-
-
-def beep_alarm():
-    for _ in xrange(30):
-        beep(0.1)
-        time.sleep(0.1)
-
-
-def beep_success():
-    for _ in xrange(2):
-        beep(0.2)
-        time.sleep(0.2)
-
-
-def beep_fail():
-    beep(1)
 
 if __name__ == '__main__':
     main()
