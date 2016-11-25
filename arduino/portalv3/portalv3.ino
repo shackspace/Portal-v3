e8f1c42c3e9a57bfa0b40b028e5373b953bc30fd
M1k3y-shack <tobias1212@go4more.de>
2016-11-25 22:22:49 +0100

    made communication routine non-blocking when waiting and added debouncing for the close-button

--- arduino/portalv3/portalv3.ino
+++ arduino/portalv3/portalv3.ino
@@ -21,18 +21,91 @@ void setup()
   
 }
 
+unsigned long target_timeout;  // target for timeouts
+int button_state = 0;       // 0 = idle, button not pushed
+                            // 1 = checking, check if button is pushed, check if it was just a random signal or intended
+                            // 2 = closing, wait for raspi to read button state or until the timeout is reached
+                            // 3 = wait, wait until the button is released
+
 void loop()
 {
-  int comm1 = Serial.parseInt();
-  int comm2 = Serial.parseInt();
-  if(Serial.read() == '\n')
+  if(Serial.available() > 0)
   {
-    parseCMD(comm1, comm2);
+    int comm1 = Serial.parseInt();
+    int comm2 = Serial.parseInt();
+    if(Serial.read() == '\n')
+    {
+      parseCMD(comm1, comm2);
+    }
   }
-  if(!digitalRead(closebutton))
+
+  switch(button_state)
   {
-    closing_requested = 1;
+    case 0:   // idle
+      closing_requested = 0;
+      if(!digitalRead(closebutton)) // button pushed
+      {
+        target_timeout = millis() + 100;  // set target 100 milliseconds into the future
+        button_state = 1;
+      }
+      break;
+    case 1:   // checking
+      if(digitalRead(closebutton))  // button_released
+      {
+        button_state = 0;   // return to idle
+      }
+      else
+      {
+        if(check_timer())   // timeout reached
+        {
+          target_timeout = millis() + 10000; // set target 10 seconds into the future
+          closing_requested = 1;
+          button_state = 2; // start closing
+        }
+      }
+      break;
+    case 2:   // closing
+      if(closing_requested == 0)  // the request was read by the raspi
+      {
+        button_state = 4;   // start waiting
+      }
+      else
+      {
+        if(check_timer())   // timed out waiting for the raspi. clear request flag and go to idle
+        {
+          closing_requested = 0;
+          button_state = 0;
+        }
+      }
+      break;
+    case 4:   // wait
+      if(digitalRead(closebutton))  // button is released
+      {
+        button_state = 0;
+      }
+      break;
   }
+
+  
+}
+
+bool check_timer()
+{
+  if(millis() >= target_timeout)  // current time is greater than the target time
+  {
+    if(target_timeout < 1000000)  // the target time is smaller than 1000 seconds
+    {
+      if(millis() < 2000000)      // if the current time is smaller than 2000 seconds we can be sure it's not greater than the target because the target overflow but because it really is
+      {
+        return true;
+      }
+    }
+    else  // no need to check for overflow problems
+    {
+      return true;
+    }
+  }
+  return false;
 }
 
 void parseCMD(int comm1, int comm2)
